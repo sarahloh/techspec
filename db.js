fa41112400918464b96d0dc888c957b8491e123e
sloh <sloh@tssg.org>
2014-06-11 09:55:08 +0100

    Experimenting with mongodb

--- db.js
+++ db.js
@@ -4,4 +4,14 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback(){
   console.log('Connected to db!');
+
+  var userSchema = mongoose.Schema({
+    name: String
+  });
+  var User = mongoose.model('User', userSchema);
+  var user1 = new User({name: 'Molly'});
+  user1.save(function(err, user1) {
+    if (err) return console.error(err);
+    console.info(user1.name + ' added!');
+  });
 });
