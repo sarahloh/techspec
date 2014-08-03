d34cd1d7501b4f5c18d899fe610c4dbfa325bc51
sloh <sloh@tssg.org>
2014-08-03 15:44:50 +0100

    Adding findById

--- routes/user.js
+++ routes/user.js
@@ -4,66 +4,49 @@ var mongoose = require('mongoose'),
       config = require('./../config/config.js');
 
 mongoose.connect(config.database_url, function(){
-  console.log('connected!');
+  console.log('connected to db');
   mongoose.connection.db.dropDatabase(function(){
     models.User.collection.insert(testUsers.users, function(){
       console.log('users inserted');
     });
   });
 });
-//var models = require('./../models')(mongoose);
-//models.User.collection.insert(testUsers.users, function(){
-//  console.log('users inserted');
-//});
 
 exports.login = function(req, res){
-  var u = req.body;
+  var r = req.body;
   res.send({
-    email     :  u.email,
-    password  :  u.password
+    email     :  r.email,
+    password  :  r.password
   });
 };
-exports.signup = function(req, res){
-  var u = req.body;
 
+exports.signup = function(req, res){
+  var r = req.body;
   var user = new models.User({
     name      :  {
-      first   :  req.body.firstName, 
-      last    :  req.body.lastName
+      first   :  r.firstName, 
+      last    :  r.lastName
     }, 
-    email     :  req.body.email,
-    password  :  req.body.password
+    email     :  r.email,
+    password  :  r.password
   });
-
   user.save(function(err, user){
     if(err) return console.error(err);
   });
- 
-  res.send({
-    name      :  {
-      first   :  req.body.firstName, 
-      last    :  req.body.lastName
-    }, 
-    email     :  req.body.email,
-    password  :  req.body.password
-  });
 };
-exports.findAll = function(req, res){
 
+exports.findAll = function(req, res){
   models.User.find(function(err, users){
     if(err) return console.error(err);
-    console.log(users);
+//    console.log(users);
     res.send(users);
   });
-
-/*
-  res.send(models.Users.find(function(err, users){
-    if(err) return console.error(err);
-    return users;
-  }));
-*/
-//  res.send([{name: 'user1'}, {name: 'user2'}]);
 };
+
 exports.findById = function(req, res){
-  res.send({id:req.params.id, name: 'Sarah Loh'});
+  models.User.findById(req.params.id, function(err, user){
+    if(err) return console.error(err);
+//    console.log(user);
+    res.send(user);
+  });
 };
